9a661e935944ee3fd5bf694a831880357b207e77
Leif Norcott <nibuen@gmail.com>
2014-05-07 13:43:28 -0700

    KOALA-4 #comment initial file pull of koala stuff

--- Koala-desktop/src/com/hospira/test/desktop/DesktopLauncher.java
+++ Koala-desktop/src/com/hospira/test/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.hospira.test.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.hospira.test.Koala;
+import com.hospira.test.SuperKoalio;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Koala(), config);
+		new LwjglApplication(new SuperKoalio(), config);
 	}
 }
