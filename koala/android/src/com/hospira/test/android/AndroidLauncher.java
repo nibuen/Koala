616a9ddfedaa62d21168ce899b115579e9493947
Leif Norcott <nibuen@gmail.com>
2014-05-07 13:58:58 -0700

    KOALA-4 #comment regenerated structure due to git plugin causing incorrect merge from grade project

---
+++ koala/android/src/com/hospira/test/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hospira.test.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hospira.test.Koala;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Koala(), config);
+	}
+}
