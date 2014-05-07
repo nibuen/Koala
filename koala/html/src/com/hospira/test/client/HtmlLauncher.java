70acedc514fc31d1987cc1da932bd37d2efe2265
Leif Norcott <nibuen@gmail.com>
2014-05-07 14:15:34 -0700

    KOALA-4 #comment Added fix for bad launcher type for ios and html

--- koala/html/src/com/hospira/test/client/HtmlLauncher.java
+++ koala/html/src/com/hospira/test/client/HtmlLauncher.java
@@ -3,7 +3,7 @@ package com.hospira.test.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.hospira.test.Koala;
+import com.hospira.test.SuperKoalio;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -14,6 +14,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener getApplicationListener () {
-                return new Koala();
+                return new SuperKoalio();
         }
 }
--- koala/ios/src/com/hospira/test/IOSLauncher.java
+++ koala/ios/src/com/hospira/test/IOSLauncher.java
@@ -5,13 +5,12 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.hospira.test.Koala;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new Koala(), config);
+        return new IOSApplication(new SuperKoalio(), config);
     }
 
     public static void main(String[] argv) {
